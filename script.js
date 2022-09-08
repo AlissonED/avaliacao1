b149e30f1be5dca4b9933777a55b1bd8c496f00b
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2022-09-08 09:17:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function calcMult() {
     let num1 = document.getElementsByName("n1")[0].value;
-    
-    let res = Number(num1);
-    document.getElementsByName("resultado")[0].value = res;
+    let num2 = document.getElementsByName("n2")[0].value;
+
+    let res = Number(num1) * Number(num2)
+
+    document.getElementsByName("re")[0].value = res;
 }
